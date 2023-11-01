34055bb48cdda2bf3770028f2c1317a139aa092d
maxmacauley <148719640+maxmacauley@users.noreply.github.com>
2023-11-01 12:13:36 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+let homeScoreEl = document.getElementById("home-score")
+let awayScoreEl = document.getElementById("away-score")
+
+let homeScore = 0
+let awayScore = 0
+
+function increaseHomeScoreOne(){
+    homeScore += 1
+    homeScoreEl.textContent = homeScore
+}
+
+function increaseHomeScoreTwo(){
+    homeScore += 2
+    homeScoreEl.textContent = homeScore
+}
+
+function increaseHomeScoreThree(){
+    homeScore += 3
+    homeScoreEl.textContent = homeScore
+}
+
+function increaseAwayScoreOne(){
+    awayScore += 1
+    awayScoreEl.textContent = awayScore
+}
+
+function increaseAwayScoreTwo(){
+    awayScore += 2
+    awayScoreEl.textContent = awayScore
+}
+
+function increaseAwayScoreThree(){
+    awayScore += 3
+    awayScoreEl.textContent = awayScore
+}
